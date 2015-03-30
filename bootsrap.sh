24143acae35e115faaf92700725c98ff06cf8ab3
Vladimir Ivanov <autodelete@gmail.com>
2015-03-30 00:25:19 -0700

    bootstrap - adding user to docker group

--- bootsrap.sh
+++ bootsrap.sh
@@ -4,3 +4,5 @@ sudo apt-get -y remove firefox
 sudo apt-get -y install curl wget git vim
 git clone https://github.com/autodelete/sandboxes && cd sandboxes
 ./docker-install.sh
+echo Adding $USER to docker group....
+sudo usermod -aG docker $USER
