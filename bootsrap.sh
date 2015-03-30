9749dade9af1f76db7bd6a72e332d0cee2827b49
Vladimir Ivanov <autodelete@gmail.com>
2015-03-30 00:28:56 -0700

    sudo su -l user

--- bootsrap.sh
+++ bootsrap.sh
@@ -6,3 +6,9 @@ git clone https://github.com/autodelete/sandboxes && cd sandboxes
 ./docker-install.sh
 echo Adding $USER to docker group....
 sudo usermod -aG docker $USER
+
+echo Starting docker service...
+sudo service docker start
+
+echo Executing "sudo su -l $USER" to get new group memership
+sudo su -l $USER
